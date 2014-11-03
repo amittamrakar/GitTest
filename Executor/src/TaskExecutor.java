7a2ba53e38f64744f63004de9787cf65db62ef8a
fastcoder <amitkt2020@gmail.com>
2014-11-03 09:02:07 +0530

    Added Executor
    Simple executor project

---
+++ Executor/src/TaskExecutor.java
@@ -0,0 +1,14 @@
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
+public class TaskExecutor
+{
+	private static ExecutorService taskExecutor;
+	
+	public static void main(String[] args)
+	{
+		taskExecutor = Executors.
+	}
+}
